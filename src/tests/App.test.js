ae6bb54cd0620eed6719d945040e17ff43645b91
Adolligit <adelson.onod@gmail.com>
2022-03-22 19:06:11 -0300

    Criando interação de navegação e verificação de rotas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouter } from '../RenderWithRouter';
 
 test('O topo da página possui os links: "Home", "About" e "Favorite pokémons"', () => {
-  renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
 
   const home = screen.getByRole('link', { name: /home/i });
-  const about = screen.getByRole('link', { name: /about/i });
-  const favoritePokemons = screen.getByRole('link', { name: /favorite pokémons/i });
-
   expect(home).toBeInTheDocument();
+  userEvent.click(home);
+  expect(history.location.pathname).toBe('/');
+
+  const about = screen.getByRole('link', { name: /about/i });
   expect(about).toBeInTheDocument();
+  userEvent.click(about);
+  expect(history.location.pathname).toBe('/about');
+
+  const favoritePokemons = screen.getByRole('link', { name: /favorite pokémons/i });
   expect(favoritePokemons).toBeInTheDocument();
+  userEvent.click(favoritePokemons);
+  expect(history.location.pathname).toBe('/favorites');
 });
