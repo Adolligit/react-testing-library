ce24a4d08946c5d89a0523deee41a9f324f407fd
Adolligit <adelson.onod@gmail.com>
2022-03-22 14:33:55 -0300

    Iniciando teste do topo  da aplicação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+test('O topo da aplicação contém um conjunto fixo de links de navegação: "Home", "About" e "Favorite pokémons"', () => {
+  render(<App />);
+});
