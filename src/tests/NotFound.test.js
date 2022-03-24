c8753756c424b6772a5c7f257f039f78fc633c3b
Adolligit <adelson.onod@gmail.com>
2022-03-24 15:43:04 -0300

    Verificando se há os elementos da rota 'NotFound' (h2 e img)

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import { renderWithRouter } from '../RenderWithRouter';
+
+const IMAGE_SRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+const IMAGE_ALT = 'Pikachu crying because the page requested was not found';
+
+test('A rota "Not Found" possui um h2 e uma imagem gif', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/404');
+
+  screen.getByRole('heading', { name: /Page requested not found/i });
+  const { src } = screen.getByRole('img', { name: IMAGE_ALT });
+  expect(src).toEqual(IMAGE_SRC);
+});
