9891fb3759b4ab48f4d37b569da80729da4a8425
Adolligit <adelson.onod@gmail.com>
2022-03-31 23:54:52 -0300

    Verificando a criação de dados na página de todos os Pokémons

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from '../RenderWithRouter';
 import App from '../App';
 import data from '../data';
+
 describe('Name of the group', () => {
   test('Existe uma rota para cada Pokémon de detalhes;', () => {
     const { history } = renderWithRouter(<App />);
 
-    data.forEach(({ id }, i) => { // 0123
+    data.forEach(({ id }, verifiedIds) => {
       userEvent.click(screen.getByRole('link', { name: /More details/i }));
-      console.log(id);
       expect(history.location.pathname).toEqual(`/pokemons/${id}`);
 
       userEvent.click(screen.getByRole('link', { name: /Home/i }));
-      for (let j = 0; j < (i + 1); j += 1) {
+
+      for (let howManyClicks = 0; howManyClicks < (verifiedIds + 1); howManyClicks += 1) {
+        userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
+      }
+    });
+  });
+
+  test.only('Os dados de todos os Pokémons são criados corretamente na página', () => {
+    renderWithRouter(<App />);
+
+    data.forEach((pokemon, index, { length }) => {
+      const { name, type, averageWeight: { value, measurementUnit }, image } = pokemon;
+      const { textContent: nameId } = screen.getByTestId('pokemon-name');
+      const { textContent: typeId } = screen.getByTestId('pokemon-type');
+      const { textContent: weightId } = screen.getByTestId('pokemon-weight');
+      const img = screen.getByRole('img', { name: `${name} sprite` });
+
+      expect(nameId).toEqual(name);
+      expect(typeId).toEqual(type);
+      expect(weightId).toEqual(`Average weight: ${value} ${measurementUnit}`);
+      expect(img.alt).toEqual(`${name} sprite`);
+      expect(img.src).toEqual(image);
+
+      if (index < length) {
         userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
       }
     });
