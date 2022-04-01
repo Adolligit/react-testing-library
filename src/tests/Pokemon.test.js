016881a83af878edb3f26f698f120e5a96193256
Adolligit <adelson.onod@gmail.com>
2022-04-01 17:28:50 -0300

    Cerficando-se sobre a criação da estrela ao favoritar o Pokémon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,41 +5,49 @@ import { renderWithRouter } from '../RenderWithRouter';
 import App from '../App';
 import data from '../data';
 
-describe('Name of the group', () => {
-  test('Existe uma rota para cada Pokémon de detalhes;', () => {
-    const { history } = renderWithRouter(<App />);
+test('Existe uma rota de detalhes para cada Pokémon', () => {
+  const { history } = renderWithRouter(<App />);
 
-    data.forEach(({ id }, verifiedIds) => {
-      userEvent.click(screen.getByRole('link', { name: /More details/i }));
-      expect(history.location.pathname).toEqual(`/pokemons/${id}`);
+  data.forEach(({ id }, verifiedIds) => {
+    userEvent.click(screen.getByRole('link', { name: /More details/i }));
+    expect(history.location.pathname).toEqual(`/pokemons/${id}`);
 
-      userEvent.click(screen.getByRole('link', { name: /Home/i }));
+    userEvent.click(screen.getByRole('link', { name: /Home/i }));
 
-      for (let howManyClicks = 0; howManyClicks < (verifiedIds + 1); howManyClicks += 1) {
-        userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
-      }
-    });
+    for (let howManyClicks = 0; howManyClicks < (verifiedIds + 1); howManyClicks += 1) {
+      userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
+    }
   });
+});
 
-  test.only('Os dados de todos os Pokémons são criados corretamente na página', () => {
-    renderWithRouter(<App />);
+test('Os dados de todos os Pokémons são criados corretamente na página', () => {
+  renderWithRouter(<App />);
 
-    data.forEach((pokemon, index, { length }) => {
-      const { name, type, averageWeight: { value, measurementUnit }, image } = pokemon;
-      const { textContent: nameId } = screen.getByTestId('pokemon-name');
-      const { textContent: typeId } = screen.getByTestId('pokemon-type');
-      const { textContent: weightId } = screen.getByTestId('pokemon-weight');
-      const img = screen.getByRole('img', { name: `${name} sprite` });
+  data.forEach((pokemon, index, { length }) => {
+    const { name, type, averageWeight: { value, measurementUnit }, image } = pokemon;
+    const { textContent: nameId } = screen.getByTestId('pokemon-name');
+    const { textContent: typeId } = screen.getByTestId('pokemon-type');
+    const { textContent: weightId } = screen.getByTestId('pokemon-weight');
+    const img = screen.getByRole('img', { name: `${name} sprite` });
 
-      expect(nameId).toEqual(name);
-      expect(typeId).toEqual(type);
-      expect(weightId).toEqual(`Average weight: ${value} ${measurementUnit}`);
-      expect(img.alt).toEqual(`${name} sprite`);
-      expect(img.src).toEqual(image);
+    expect(nameId).toEqual(name);
+    expect(typeId).toEqual(type);
+    expect(weightId).toEqual(`Average weight: ${value} ${measurementUnit}`);
+    expect(img.alt).toEqual(`${name} sprite`);
+    expect(img.src).toEqual(image);
 
-      if (index < length) {
-        userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
-      }
-    });
+    if (index < length) {
+      userEvent.click(screen.getByRole('button', { name: /Próximo pokémon/i }));
+    }
   });
+});
+
+test('Validando atributo "src" e "alt" da imagem ao favoritar o Pokémon', () => {
+  renderWithRouter(<App />);
+
+  userEvent.click(screen.getByRole('link', { name: /More details/i }));
+  userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+  const { src } = screen.getByRole('img', { name: /Pikachu is marked as favorite/ });
+
+  expect(src).toEqual('http://localhost/star-icon.svg');
 });
